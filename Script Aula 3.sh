59ff94571a85ab7237fb29ee272bbda97be302d0
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-06-17 18:48:29 +0100

    Subindo repositório local de aulas pra estudar no mac

---
+++ Script Aula 3.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd Aula 3
+pwd
+cd Aula 3
+cd Aulas Digital House/Aula 3
+mkdir dh
+cd dh
+mkdir integracao_aula_1e2
+cd  integracao_aula_1e2
+mkdir historia_da_contabilidade servidores interfaces_de_usuario o_que_faremos_hoje infraestrutura
+ls
+cd servidores
+touch resumo.txt
+ls
+cp resumo.txt ../historia_da_contabilidade
+cp resumo.txt ../infraestrutura
+cp resumo.txt ../interface_de_usuario
+cp resumo.txt ../o_que_faremos_hoje
+cd ..
+19  ls
+20  rmv interface_de_usuario
+21  rm interface_de_usuario
+ls
+cp resumo.txt ../interfaces_de_usuario
+cd servidores
+cp resumo.txt ../interfaces_de_usuario
+cd ..
+ls
+cd ..
+ls -R
