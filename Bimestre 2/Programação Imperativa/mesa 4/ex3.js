7c82888011654340bb35c98083d79be924012dd6
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-08-10 02:52:44 +0100

    Aula Programação Imperativa dia 10-08-2022

---
+++ Bimestre 2/Programação Imperativa/mesa 4/ex3.js
@@ -0,0 +1,34 @@
+
+// Exercício 4
+
+var x = 10;
+var y = 20;
+
+var z = x + y;
+
+console.log(x + ' + ' + y + ' = ' + z);
+
+var x = 10;
+var y = 20;
+
+var z = x * y;
+
+console.log(x + ' * ' + y + ' = ' + z);
+
+
+// Exercício 5
+
+var nome = "Lucas";
+var idade = 25;
+var peso = 95;
+var altura = 1.82;
+var plano = "Tem";
+var IMC = peso / (altura * altura);
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Peso: " + peso + " Kg");
+console.log("Altura: " + altura + " m");
+console.log("Plano: " + plano);
+console.log("IMC: " + IMC);
+console.log(`${nome} possui ${idade} anos, pesa ${peso}Kg, e tem ${altura}m de altura. o seu IMC é ${IMC}`);
