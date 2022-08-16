90941f35ce837372ac6c7722f7ff655635216812
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-08-16 02:29:21 +0100

    Mesa de Programação Imperativa do dia 15/08/2022

---
+++ Bimestre 2/Programação Imperativa/mesa_16-08-2022.js
@@ -0,0 +1,38 @@
+// console.log(!true)
+// console.log(!false)
+// console.log(!!false)
+// console.log(!!true)
+// console.log(!1)
+// console.log(!!1)
+// console.log(!0)
+// console.log(!!0)
+// console.log(!'')
+
+// let x = 5
+
+// let y = 9
+
+// console.log(x < 10 && x !== 5)
+// console.log(x > 9 || x !== 5)
+// console.log(!(x == y))
+
+
+//EXERCÍCIO 2   ***************************
+
+// let x = 10
+// let y = "a"
+// console.log(y === "b" || x >= 10)
+
+
+// let x=3
+// let y=8
+// !(x == "3" || x === y) && !(y !== 8 && x <= y)
+
+// let str = ""
+// let msg = "haha!"
+// let eBonito = "false"
+// console.log(!((str || msg) && eBonito))
+
+
+
+
