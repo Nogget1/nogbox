4b24c25b99b0e9472bf2e47f4c378f65e259c4c6
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-08-24 18:03:42 +0100

    mesa de trabalho do dia 22-08-2022

---
+++ Bimestre 2/Programação Imperativa/mesa_22-08-2022.js
@@ -0,0 +1,91 @@
+
+
+//Testes introdutórios
+// let numbers = [22, 33, 54, 66, 72];
+
+// console.log(numbers[numbers.length]);
+
+// grupoDeAmigos = [["Harry", "Ron", "Hermione"], ["Spiderman", "Hulk", "Ironman"], ["Penélope Glamour", "Pierre Nodoyuna", "Patán"]];
+// console.log(grupoDeAmigos[1][0]);
+
+// let str = "uma string qualquer";
+// let grupoDeAmigos = [[45, 89, 0], ["Digital", "House", true], ["string", "123", "false", 54, true, str]];
+// console.log(grupoDeAmigos[2][grupoDeAmigos[2].length - 1]);
+
+// Imprimir inverso
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+//opção 1 
+// function imprimirInverso(numbers) {
+//     let length = numbers.length;
+//     for (var i = length - 1; i >= 0; i--) {
+//         console.log(numbers[i]);
+//     }
+//     return ''
+// }
+// console.log(imprimirInverso(numbers));
+
+//Opção 2
+// console.log(numbers)
+// function imprimirInverso(array) {
+//     array.reverse();
+//     console.log(array)
+// }
+
+// imprimirInverso(numbers)
+
+//Inverter
+
+// function inverter(numbers) {
+//         numbers.reverse();
+//         console.log(numbers)
+//     }
+
+//Somar Array
+
+// function somarArray(array) {
+//     return array[0] + array[1] + array[2] + array[3]
+// }
+
+// console.log(somarArray([1, 2, 3, 0]));
+// console.log(somarArray([10, 3, 10, 4]));
+// console.log(somarArray([-5, 100, 0, 0]));
+
+//Join Array
+// let juntar = ["o", "l", "á"];
+// let juntar1 = ["t", "c", "h", "a", "u"];
+// console.log(juntar.join("") + " " + juntar1.join(""));
+
+// let filmes = ["Star Wars", "Totoro", "Rocky", "Pulp Fiction", "A Vida é bela"];
+// console.log(filmes);
+
+// function ConverterMaiusculas(array) {
+//     array[0] = array[0].toUpperCase()
+//     array[1] = array[1].toUpperCase()
+//     array[2] = array[2].toUpperCase()
+//     array[3] = array[3].toUpperCase()
+//     array[4] = array[4].toUpperCase()
+//     return array
+// }
+
+// // console.log(ConverterMaiusculas(filmes));
+
+// let filmes2 = ["Toy Story", "Procurando Nemo", "Kung-fu Panda", "Wally", "Fortnite"];
+// function juntarArrays(array1, array2) {
+//     array2.pop()
+//     arrayFinal = array1.concat(array2);
+//     return arrayFinal
+// }
+
+// console.log(juntarArrays(filmes, filmes2));
+
+// console.log(arrayFinal);
+// arrayFinalFinal = arrayFinal.map(p => p.toUpperCase());
+// console.log(arrayFinalFinal);
+
+//Compare arrays
+
+const asiaScores = [8, 10, 6, 9, 10, 6, 6, 8, 4];
+const euroScores = [8, 10, 6, 8, 10, 6, 7, 9, 5];
+console.log(asiaScores === euroScores)
+
