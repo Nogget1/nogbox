5b095e87de1119e6e37b3cbd1a3fe4daf377e601
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-10-19 18:54:19 +0100

    Mesa 18/10 e pasta de front 3

---
+++ Bimestre 3/Front-End3/mesa1/index1.js
@@ -0,0 +1,18 @@
+
+
+var array = [1, 2, 3, 4, 5];
+var soma = 0;
+
+for (var i = 0; i < array.length; i++) {
+    soma += array[i]
+    console.log(soma);
+}
+
+
+
+// var arrey = [1,2,4,8];
+// var soma = 0;
+// for(var i = 0; i < arrey.length; i++) {
+//     soma += arrey[i]
+//     console.log(soma);
+// }
