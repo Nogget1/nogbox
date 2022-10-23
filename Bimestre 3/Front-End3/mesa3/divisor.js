144485474af2848b0e57e6477ecc6a048c0a7b2c
Lucas Nogueira <lucasnogueiradealmeida@gmail.com>
2022-10-23 21:49:11 +0100

    Mesa 3

---
+++ Bimestre 3/Front-End3/mesa3/divisor.js
@@ -0,0 +1,11 @@
+
+
+function dividir(a, b) {
+    if (a === 0 || b === 0) {
+        return console.log("Não se pode dividir por 0 ")
+    } else return a / b;
+}
+
+// console.log(dividir(0, 2));
+
+export default dividir;
